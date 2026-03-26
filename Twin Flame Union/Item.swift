e07c44e4ea7bc23adc169532407a2c070104e5d5
Justin Manuel Rodriguez II <justin04rodriguez04@gmail.com>
2026-03-25 23:55:06 -0500

    Initial Commit

---
+++ Twin Flame Union/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Twin Flame Union
+//
+//  Created by j on 3/25/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Twin Flame UnionTests/Twin_Flame_UnionTests.swift
@@ -0,0 +1,18 @@
+//
+//  Twin_Flame_UnionTests.swift
+//  Twin Flame UnionTests
+//
+//  Created by j on 3/25/26.
+//
+
+import Testing
+
+struct Twin_Flame_UnionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
